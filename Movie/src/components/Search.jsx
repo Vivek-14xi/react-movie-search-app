4d70b04c766d103193ac8e064f14ba4501bca925
Vivek Dashariye <vivekdashariye2005@gmail.com>
2026-03-23 23:54:53 +0530

    updating

--- Movie/src/components/Search.jsx
+++ Movie/src/components/Search.jsx
@@ -2,11 +2,18 @@ import React from 'react'
 import { useState } from 'react'
 import "./Search.css"
 export default function Search() {
-  
+  const[name,setName]=useState("batman")
+  function handleSubmit(e){
+    e.preventDefault();
+  }
+  function handleChange(e){
+    setname(e.target.value)
+
+  }
   return (
     <div>
-      <form  className="search">
-        <input type="text"   className="search input"/>
+      <form  className="search" onSubmit={handleSubmit}>
+        <input type="text" value={name}  className="search input" onChange={handleChange}/>
         <button className="search button" type="submit">Search</button>
       </form>
     </div>
